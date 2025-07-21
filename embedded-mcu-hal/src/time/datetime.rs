f71eaab9b429af3deed0628d06da2275999e9aa5
Billy Price <williamp@microsoft.com>
2025-07-21 15:34:20 -0700

    Add tests for conversions

--- embedded-mcu-hal/src/time/datetime.rs
+++ embedded-mcu-hal/src/time/datetime.rs
@@ -219,7 +219,7 @@ impl Datetime {
             return Err(DatetimeError::Second);
         }
 
-        if data.nanosecond >= 999_999_999 {
+        if data.nanosecond > 999_999_999 {
             return Err(DatetimeError::Nanosecond);
         }
 
@@ -321,5 +321,438 @@ impl From<Datetime> for chrono::NaiveDateTime {
             date_time.data.nanosecond,
         )
         .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(all(test, feature = "defmt"))]
+    compile_error!("Tests don't work with defmt enabled - try `cargo test --no-default-features`.");
+
+    fn verify_unix_timestamp_roundtrip(data: UncheckedDatetime) {
+        let dt = Datetime::new(data).expect("Datetime should be valid");
+        let secs = dt.to_unix_time_seconds();
+        let dt2 = Datetime::from_unix_time_seconds(secs);
+        assert_eq!(dt, dt2, "Datetime roundtrip failed for {:?}", data);
+    }
+
+    #[test]
+    fn test_unix_timestamp_roundtrip() {
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 1979,
+            month: 1,
+            day: 1,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2023,
+            month: 10,
+            day: 4,
+            hour: 16,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 3,
+            day: 2,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 3,
+            day: 1,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 2,
+            day: 29,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 2,
+            day: 28,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 1,
+            day: 31,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 1,
+            day: 1,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 2,
+            day: 1,
+            ..Default::default()
+        });
+
+        verify_unix_timestamp_roundtrip(UncheckedDatetime {
+            year: 2024,
+            month: 10,
+            day: 4,
+            hour: 16,
+            ..Default::default()
+        });
+    }
+
+    #[test]
+    fn test_datetime_bounds() {
+        // Years
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 1969,
+                month: 1,
+                day: 1,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Year)
+        );
+
+        // Months
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 1970,
+                month: 0,
+                day: 1,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Month)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 1970,
+                month: 0,
+                day: 1,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Month)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 1970,
+                month: 13,
+                day: 1,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Month)
+        );
+
+        // Leap year stuff
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2100,
+                month: 2,
+                day: 29,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2015,
+                month: 2,
+                day: 29,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2000,
+            month: 2,
+            day: 29,
+            ..Default::default()
+        }) {
+            assert!(false, "2000-02-29 should be a valid date");
+        }
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2004,
+            month: 2,
+            day: 29,
+            ..Default::default()
+        }) {
+            assert!(false, "2004-02-29 should be a valid date");
+        }
+
+        // Normal Days
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 0,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 32,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 32,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 9,
+                day: 31,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Day)
+        );
+
+        // Hours
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 1,
+                hour: 24,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Hour)
+        );
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2025,
+            month: 1,
+            day: 1,
+            hour: 23,
+            ..Default::default()
+        }) {
+            assert!(false, "23 should be a valid hour");
+        }
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2025,
+            month: 1,
+            day: 1,
+            hour: 0,
+            ..Default::default()
+        }) {
+            assert!(false, "23 should be a valid hour");
+        }
+
+        // Minutes
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 1,
+                hour: 0,
+                minute: 60,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Minute)
+        );
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2025,
+            month: 1,
+            day: 1,
+            hour: 0,
+            minute: 59,
+            ..Default::default()
+        }) {
+            assert!(false, "59 should be a valid minute");
+        }
+
+        // Seconds
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 1,
+                hour: 0,
+                minute: 59,
+                second: 60,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Second)
+        );
+
+        // This is an actual leap second, but we don't support leap seconds in the Datetime type.
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2016,
+                month: 12,
+                day: 31,
+                hour: 23,
+                minute: 59,
+                second: 60,
+                ..Default::default()
+            }),
+            Err(DatetimeError::Second)
+        );
+
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2025,
+            month: 1,
+            day: 1,
+            hour: 0,
+            minute: 59,
+            second: 59,
+            ..Default::default()
+        }) {
+            assert!(false, "59 should be a valid second");
+        }
+
+        // Nanoseconds
+        if let Err(_) = Datetime::new(UncheckedDatetime {
+            year: 2025,
+            month: 1,
+            day: 1,
+            hour: 0,
+            minute: 59,
+            second: 59,
+            nanosecond: 999_999_999,
+        }) {
+            assert!(false, "999_999_999 should be a valid second");
+        }
+
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2025,
+                month: 1,
+                day: 1,
+                hour: 0,
+                minute: 59,
+                second: 59,
+                nanosecond: 1_000_000_000,
+            }),
+            Err(DatetimeError::Nanosecond)
+        );
+
+        // This is how chrono represents leap seconds.  This was an actual leap second, but we don't support leap seconds in the Datetime type.
+        assert_eq!(
+            Datetime::new(UncheckedDatetime {
+                year: 2016,
+                month: 12,
+                day: 31,
+                hour: 23,
+                minute: 59,
+                second: 59,
+                nanosecond: 1_999_999_999,
+            }),
+            Err(DatetimeError::Nanosecond)
+        );
+    }
+
+    #[test]
+    #[cfg(feature = "chrono")]
+    fn test_chrono_conversion() {
+        use chrono::{NaiveDate, NaiveDateTime};
+
+        {
+            let dt = Datetime::new(UncheckedDatetime {
+                year: 2023,
+                month: 10,
+                day: 4,
+                hour: 16,
+                minute: 30,
+                second: 45,
+                nanosecond: 123_456_789,
+            })
+            .expect("Datetime should be valid");
+
+            let chrono_dt: NaiveDateTime = dt.into();
+            assert_eq!(
+                chrono_dt,
+                NaiveDate::from_ymd_opt(2023, 10, 4)
+                    .expect("Should be a valid NaiveDate")
+                    .and_hms_nano_opt(16, 30, 45, 123_456_789)
+                    .expect("Should be a valid NaiveTime")
+            );
+
+            let dt_from_chrono = Datetime::try_from(chrono_dt).expect("Should convert back to Datetime");
+            assert_eq!(dt, dt_from_chrono);
+        }
+
+        {
+            let chrono_leap_dt: NaiveDateTime = NaiveDate::from_ymd_opt(2016, 12, 31)
+                .expect("Should be a valid NaiveDate")
+                .and_hms_nano_opt(23, 59, 59, 1_999_999_999)
+                .expect("NaiveDateTime has partial support for leap seconds - this should be a valid NaiveTime, but not a valid Datetime. Truncation is expected in conversion.");
+
+            let dt_from_chrono_leap = Datetime::try_from(chrono_leap_dt);
+            assert_eq!(
+                dt_from_chrono_leap,
+                Datetime::new(UncheckedDatetime {
+                    year: 2016,
+                    month: 12,
+                    day: 31,
+                    hour: 23,
+                    minute: 59,
+                    second: 59,
+                    nanosecond: 999_999_999, // We expect that the 'overflow nanoseconds' from the leap second have been truncated in the conversion.
+                })
+            );
+        }
+
+        {
+            let chrono_early_dt: NaiveDateTime = NaiveDate::from_ymd_opt(1969, 12, 31)
+                .expect("Should be a valid NaiveDate")
+                .and_hms_nano_opt(23, 59, 59, 999_999_999)
+                .expect("Should be a valid NaiveTime");
+
+            assert_eq!(Datetime::try_from(chrono_early_dt), Err(DatetimeError::Year));
+        }
+    }
+
+    #[test]
+    fn test_unix_time_conversion() {
+        let dt = Datetime::from_unix_time_seconds(0);
+        assert_eq!(dt.year(), 1970);
+        assert_eq!(dt.month(), 1);
+        assert_eq!(dt.day(), 1);
+        assert_eq!(dt.hour(), 0);
+        assert_eq!(dt.minute(), 0);
+        assert_eq!(dt.second(), 0);
+        assert_eq!(dt.nanoseconds(), 0);
+
+        let dt = Datetime::from_unix_time_seconds(86400); // 1 day later
+        assert_eq!(dt.year(), 1970);
+        assert_eq!(dt.month(), 1);
+        assert_eq!(dt.day(), 2);
+        assert_eq!(dt.hour(), 0);
+        assert_eq!(dt.minute(), 0);
+        assert_eq!(dt.second(), 0);
+        assert_eq!(dt.nanoseconds(), 0);
     }
 }
